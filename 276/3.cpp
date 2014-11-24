687d17c655d6b377ce8c3287e2b9b3f35f1dfd60
Rituraj <rituraj.tc@gmail.com>
2014-11-24 08:22:26 +0530

    D. Unbearable Controversy of Being

--- 276/3.cpp
+++ 276/3.cpp
@@ -17,9 +17,68 @@ const double eps    =   1e-8;
 /*Solution code starts here */
 
 
+#define maxn 3005
+
+int  all[maxn][maxn];
+
+bool done[maxn][maxn];
+
+vector<int> graf[maxn];
+
+void dfs( int curr ,int p)
+{
+    if( done[curr][p] )
+         return;
+
+    done[curr][p]=true;
+
+    for(int i=0;i<sz( graf[curr]);i++)
+    {
+        int nx=graf[curr][i];
+
+        if(nx==p)
+            continue;
+        all[p][nx]++;
+
+        dfs(nx,curr);
+    }
+
+}
+
 int main()
 {
  ios_base::sync_with_stdio(0);
+
+ int n,m;
+
+ cin>>n>>m;
+
+ int a,b;
+
+ while(m--)
+ {
+     cin>>a>>b;
+     graf[a].pb(b);
+ }
+
+ fill(done,0);
+ fill(all,0);
+
+ for(int i=1;i<=n;i++)
+    for(int j=0;j<sz( graf[i] );j++)
+      dfs( graf[i][j],i); //for every edge
+
+ ll ans=0;
+
+ for(int i=1;i<=n;i++)
+    for(int j=1;j<=n;j++)
+ {
+     ll cc= all[i][j];
+
+     ans+=cc*(cc-1)/2LL;
+ }
+
+ cout<<ans<<endl;
 
  return 0;
 
