81247dba89ffda01c0dc08616b44bd4938f2ff6b
Rituraj <rituraj.tc@gmail.com>
2014-12-23 08:38:45 +0530

    greater string

--- 279/2.cpp
+++ 279/2.cpp
@@ -19,7 +19,7 @@ const double eps    =   1e-8;
 
 
 
-pair<ll,ll> get_cont( ll a)
+pair<ll,ll> get_cont( ll a)//return number of  2's & 3's
 {
     pair<ll,ll> ans=mp(0,0);
 
@@ -32,12 +32,59 @@ pair<ll,ll> get_cont( ll a)
     while( (a>1) && (a%3 == 0))
     {
         ans.Y++;
-        a=a/2LL;
+        a=a/3LL;
     }
 
     return ans;
 }
 
+
+pair<ll,ll> solve2( ll a , ll b , ll cont)
+{
+    while( cont > 0)
+    {
+         if( a % 2LL ==0 )
+         {
+             a =a/2LL;
+             cont--;
+             continue;
+         }
+
+         if( b % 2LL ==0)
+         {
+             b = b/2LL;
+             cont--;
+             continue;
+         }
+    }
+
+    return mp(a,b);
+}
+
+
+pair<ll,ll> solve3( ll a , ll b , ll cont)
+{
+    while( cont > 0)
+    {
+         if( a % 3LL ==0 )
+         {
+             a =a/3LL;
+             a= a*2LL;
+             cont--;
+             continue;
+         }
+
+         if( b % 3LL ==0)
+         {
+             b = b/3LL;
+             b=b*2LL;
+             cont--;
+             continue;
+         }
+    }
+
+    return mp(a,b);
+}
 int main()
 {
  ios_base::sync_with_stdio(0);
@@ -59,26 +106,49 @@ int main()
 
  pair<ll,ll> A = get_cont(a*b) , B = get_cont(c*d);
 
- int cont=(1<<20);
+// cout<<A.X<<"  "<<A.Y<<"  "<<B.X<<"  "<<B.Y<<endl;
+
+
+ ll p=(1e10),q=(1e10),r=(1e10),s=(1e10);
 
  for(int i=0;i<=A.Y;i++)//3 cont in
     for(int j=0;j<=A.X + i; j++)//2 cont
       for(int k=0;k <= B.Y; k++)// 3 cont in other number
-        for(int l=0;l <=B.Y+k; l++) //2 count in other
+        for(int l=0;l <=B.X+k; l++) //2 count in other
         {
+            ll  x = (((a*b)/div3[i])*mul3[i])/div2[j];
 
+            ll  y =  (((c*d)/div3[k])*mul3[k])/div2[l];
+
+            if( x == y )
+            {
+                if( i + j + k + l < p + q + r +s )
+                {
+                    p = i; q = j ; r=k ; s= l;
+                }
+            }
         }
 
 
-   if( cont == (1<<20))
-    cout<<"-1"<<endl;
-   else
+    if( p == (1e10))
     {
-        cout<<cont<<endl;
-        cout<<a<<" "<<b<<endl;
-        cout<<c<<" "<<d<<endl;
+        cout<<"-1\n";
+        return 0;
     }
 
+    cout<<p+q+r+s<<endl;
+
+
+   pair<ll,ll> ap=solve3(a,b,p);
+               ap=solve2( ap.X , ap.Y ,q);
+
+
+   pair<ll , ll > cp =solve3(c,d,r);
+                  cp=solve2(cp.X , cp.Y ,s);
+
+
+   cout<<ap.X<<" "<<ap.Y<<endl;
+   cout<<cp.X<<" "<<cp.Y<<endl;
    return 0;
 
 }
--- 279/3.cpp
+++ 279/3.cpp
@@ -16,10 +16,92 @@ const double eps    =   1e-8;
 #define fill(a,val) memset(a ,val, sizeof(a) );
 /*Solution code starts here */
 
+#define maxn  100009
+
+string in[maxn];
+
+string solve( string curr , string prev)
+{
+    int cs=sz(curr) , ps=sz(prev);
+
+    if( cs < ps )
+         return "*";
+
+    if( cs > ps )
+    {
+         for(int i=1;i<cs;i++)
+             if( curr[i]=='?')
+                curr[i]='0';
+
+         if( curr[0]=='?')
+             curr[0]='1';
+
+         return curr;
+    }
+
+    for(int pos = cs; pos >=0; pos--)//increare value of curr[pos] by prev[pos] + 1
+    {
+         string temp=curr;
+
+         if( (pos < cs) && (prev[pos] =='9') )
+            continue;
+
+         if( (pos < cs) && (temp[pos]=='?') )
+            temp[pos]=prev[pos] + 1;
+
+         for(int i=pos+1;i<cs;i++)
+             if( temp[i]=='?')
+               temp[i]='0';
+
+         bool ans=true;
+
+         for(int i=0;i<pos;i++)
+             if( temp[i]=='?')
+                temp[i]=prev[i];
+             else if( temp [i] != prev[i])
+                 ans=false;
+
+         if(!ans)
+            continue;
+
+         if( temp > prev )
+            return temp;
+    }
+
+
+    return "*";
+}
 
 int main()
 {
  ios_base::sync_with_stdio(0);
+
+ int n;
+
+ cin>>n;
+
+ in[0]="0";
+
+ for(int i=1;i<=n;i++)
+    cin>>in[i];
+
+  for(int i=1;i<=n;i++)
+  {
+      string curr=solve( in[i],in[i-1]);
+
+      if( curr =="*")
+      {
+          cout<<"NO"<<endl;
+          return 0;
+      }
+
+      in[i]=curr;
+  }
+
+  cout<<"YES"<<endl;
+
+  for(int i=1;i<=n;i++)
+    cout<<in[i]<<endl;
 
  return 0;
 
