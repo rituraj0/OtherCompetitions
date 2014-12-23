8744a56926e3cb97dddaba18f05c98678d546abb
Rituraj <rituraj.tc@gmail.com>
2014-12-23 21:36:03 +0530

    update

--- 277/4.cpp
+++ 277/4.cpp
@@ -17,9 +17,64 @@ const double eps    =   1e-8;
 /*Solution code starts here */
 
 
+ll sum , len;
+
 int main()
 {
  ios_base::sync_with_stdio(0);
+
+
+
+ cin>>len>>sum;
+
+ if( len ==1)
+ {
+     if( (0<=sum)&&(sum<=9))
+     {
+         cout<<sum<<" "<<sum<<endl;
+         return 0;
+     }
+
+     cout<<"-1 -1"<<endl;
+     return 0;
+ }
+
+ ll mins=1,maxs=len*9LL;
+
+ if( (sum < mins) || (maxs < sum))
+ {
+     cout<<"-1 -1"<<endl;
+     return 0;
+ }
+
+ string mini=string(len,'0');
+ string maxi=mini;
+
+ maxi[0]=mini[0]='1';
+
+ //build mini
+ int tp=sum-1;
+
+ for(int i=len-1;i>=0;i--)
+      {
+         int add = min(9,tp);
+         mini[i]=mini[i] + add;
+         tp-=add;
+      }
+
+ //build maxi
+
+ tp = sum -1;
+
+ for(int i=0;i<len;i++)
+ {
+     int add= min( tp , 9-(maxi[i]-'0'));
+     maxi[i]=maxi[i]+add;
+     tp-=add;
+ }
+
+ cout<<mini<<" "<<maxi<<endl;
+
 
  return 0;
 
--- 279/1.cpp
+++ 279/1.cpp
@@ -17,9 +17,85 @@ const double eps    =   1e-8;
 /*Solution code starts here */
 
 
+string in;
+ll a,b;
+
+bool ac[1<<20],bc[1<<20];
+
 int main()
 {
  ios_base::sync_with_stdio(0);
+
+ fill(ac,0);
+ fill(bc,0);
+
+  cin>>in;
+
+  cin>>a>>b;
+
+  ll prem=0;
+  ll pp=1;
+
+  for(int i=sz(in)-1;i>=0;i--)
+  {
+      ll mul=(10LL*pp)%b;
+
+      if(i==sz(in)-1)
+        mul=1;
+
+      //cout<<i<<"  "<<mul<<"  ";
+
+      pp=mul;
+
+      ll crem= ( mul*(in[i]-'0') + prem)%b;
+
+     // cout<<prem<<"  "<<crem<<endl;
+
+      if( crem==0)
+        if( in[i]!='0')
+       {
+           bc[i]=true;
+
+          // cout<<"here we got "<<i<<endl;
+       }
+
+      prem=crem;
+  }
+
+  //cout<<"Now in A part "<<endl;
+
+
+  prem = 0;
+
+  for(int i=0;i<sz(in)-1;i++)
+  {
+      ll crem = ( 10LL*prem + ( in[i]-'0') )%a;
+
+      if( crem %a == 0)
+      {
+          ac[i]=true;
+
+         // cout<<"Here in second string we have got "<<i<<endl;
+      }
+
+      prem=crem%a;
+  }
+
+  for(int i=0;i<sz(in)-1;i++)
+      if( ac[i] && bc[i+1] )
+        {
+           cout<<"YES"<<endl;
+
+            cout<<in.substr(0,i+1)<<endl;
+
+            cout<<in.substr(i+1)<<endl;
+
+            return 0;
+        }
+
+
+
+        cout<<"NO"<<endl;
 
  return 0;
 
